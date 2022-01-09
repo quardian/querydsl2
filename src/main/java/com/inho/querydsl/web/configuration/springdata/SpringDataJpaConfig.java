4acffb04b446b893d360a7bf9af414a8b6952a40
leeinho <leeinho@soleo.kr>
2022-01-09 11:53:27 +0900

    querydsl config

--- src/main/java/com/inho/querydsl/web/configuration/springdata/SpringDataJpaConfig.java
+++ src/main/java/com/inho/querydsl/web/configuration/springdata/SpringDataJpaConfig.java
@@ -1,12 +1,15 @@
 package com.inho.querydsl.web.configuration.springdata;
 
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -16,11 +19,27 @@ import java.util.UUID;
 @EnableJpaRepositories(basePackages = {"com.inho.querydsl.web.repository"})
 public class SpringDataJpaConfig {
 
+    @PersistenceContext
+    private EntityManager em;
+
+    /**
+     * Spring Data JPA
+     * @return
+     */
     @Bean
     public AuditorAware<String> auditorProvider()
     {
         // 실제는 : 세션정보에서 ID 를 갖어와서 반환
         return () -> Optional.of( UUID.randomUUID().toString() );
+    }
+
+    /**
+     * JPAQueryFactory 빈 등록
+     * @return
+     */
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(){
+        return new JPAQueryFactory((this.em));
     }
 
 }
--- src/test/java/com/inho/querydsl/basic/QuerydslBasicTest.java
+++ src/test/java/com/inho/querydsl/basic/QuerydslBasicTest.java
@@ -1,14 +1,12 @@
 package com.inho.querydsl.basic;
 
 import com.inho.querydsl.entity.*;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
